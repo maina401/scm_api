1570cde0a327f370669086e9c548a84cf25b9516
MoffatMugwanjira <etg@t>
2023-07-06 15:20:44 +0300

    FIX: list_item_events

--- src/Models/ItemEvent.php
+++ src/Models/ItemEvent.php
@@ -5,13 +5,29 @@ namespace Leaf\Models;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Leaf\Traits\HasUUID;
 
-class ItemEvent extends Model
+class   ItemEvent extends Model
 {
 
     protected $table = 'item_events';
+    protected $casts = [
+        'item_id' => 'string',
+        'event_id' => 'string',
+        'created_by' => 'string',
+    ];
     //belongs to user
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class,'created_by');
+    }
+
+    //belongs to item
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class,'item_id');
+    }
+    //belongs to event
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class,'event_id');
     }
 }
--- src/Repositories/item_events_repository.php
+++ src/Repositories/item_events_repository.php
@@ -1,36 +1,37 @@
 <?php
+
 namespace Leaf\Repositories;
+
 use Exception;
 use Leaf\Helpers\Validator;
+use Leaf\Models\Event;
+use Leaf\Models\ItemEvent;
 use Leaf\Models\User;
 
 class item_events_repository
 {
-   //list_item_events
+    //list_item_events
+    /**
+     * @throws Exception
+     */
     public function list_item_events($data): array
     {
         Validator::make($data, [
             'item_id' => 'required|uuid',
         ]);
-        $item=User::find(session()->get('user_id'))->items()->findOrFail($data->item_id);
-         $item_events = $item->events()
-             ->get([
-                 "events.id",
-                "events.title",
-                "events.description",
-                "item_events.value"
-         ]);
-
-         //remove pivot
-            foreach ($item_events as $item_event){
-                unset($item_event->pivot);
-            }
-
-         return [
-                "item"=>$item->toArray(),
-                "events"=>$item_events->toArray()
-            ]
-         ;
+        $item = User::find(session()->get('user_id'))->items()->findOrFail($data->item_id);
+        //$item_id = (string) $item->id;
+        $item_events = ItemEvent::where("item_id", $item->id)
+            ->get([
+                "id",
+                "item_id",
+                "event_id",
+            ]);
+        $item_events = Event::whereIn("id", $item_events->pluck("event_id")->toArray())->get();
+        return [
+            "item" => $item->toArray(),
+            "events" => $item_events->toArray()
+        ];
     }
 
     //create_item_event
@@ -41,15 +42,16 @@ class item_events_repository
             'event_id' => 'required|uuid',
             "value" => "required",
         ]);
-        $item=User::find(session()->get('user_id'))->items()->findOrFail($data->item_id);
+        $item = User::find(session()->get('user_id'))->items()->findOrFail($data->item_id);
 
-        $item->events()->attach($data->event_id,["value"=>$data->value]);
+        $item->events()->attach($data->event_id, ["value" => $data->value]);
         //return the item and its events
         $item_events = $item->events()->get();
         return $item_events->toArray();
     }
 
     //update_item_event
+
     /**
      * @throws Exception
      */
@@ -60,14 +62,15 @@ class item_events_repository
             'event_id' => 'required|uuid',
             "value" => "required",
         ]);
-        $item=User::find(session()->get('user_id'))->items()->findOrFail($data->item_id);
-        $item->events()->updateExistingPivot($data->event_id,["value"=>$data->value,"meta"=>$data->meta??null]);
+        $item = User::find(session()->get('user_id'))->items()->findOrFail($data->item_id);
+        $item->events()->updateExistingPivot($data->event_id, ["value" => $data->value, "meta" => $data->meta ?? null]);
         //return the item and its events
         $item_events = $item->events()->get();
         return $item_events->toArray();
     }
 
     //delete_item_event
+
     /**
      * @throws Exception
      */
@@ -77,7 +80,7 @@ class item_events_repository
             'item_id' => 'required|uuid',
             'event_id' => 'required|uuid',
         ]);
-        $item=User::find(session()->get('user_id'))->items()->findOrFail($data->item_id);
+        $item = User::find(session()->get('user_id'))->items()->findOrFail($data->item_id);
         $item->events()->detach($data->event_id);
         //return the item and its events
         $item_events = $item->events()->get();
